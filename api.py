f7e42b3d29c2e5aff784615a97baf0c739dc3cb8
Ramya S <ramya.s.mca24@gmail.com>
2025-09-26 13:14:30 +0530

    Update api.py

--- api.py
+++ api.py
@@ -81,7 +81,7 @@ app = FastAPI(
 
 origins = [
     "http://localhost:3000",       
-    "http://65.0.173.114"         
+    "http://3.109.54.160"         
 ]
 
 # Add CORS middleware for frontend integration
